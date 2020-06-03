f2d10abd30b5dd85c4a1c39114d90ebe9a305269
HwangJohn <angelic805@gmail.com>
2020-06-04 08:07:18 +0900

    FEATURE custom print 추가

--- sng_parser/utils.py
+++ sng_parser/utils.py
@@ -13,7 +13,7 @@ import functools
 import tabulate
 
 
-__all__ = ['tprint']
+__all__ = ['tprint', 'cprint']
 
 
 def tprint(graph, file=None, show_entities=True, show_relations=True):
@@ -46,6 +46,33 @@ def tprint(graph, file=None, show_entities=True, show_relations=True):
             for rel in graph['relations']
         ]
         _print(tabulate.tabulate(relations_data, headers=['Subject', 'Relation', 'Object'], tablefmt=_tabulate_format))
+
+def cprint(graph, file=None, show_entities=True, show_relations=True):
+    """
+    Print a scene graph as a table.
+    The printed strings contains only essential information about the parsed scene graph.
+    """
+
+    _print = functools.partial(print, file=file)
+
+    if show_entities:
+        entities_data = [
+            [e['head'].lower(), e['span'].lower(), ','.join([ x['span'].lower() for x in e['modifiers'] ])]
+            for e in graph['entities']
+        ]
+
+    if show_relations:
+        entities = graph['entities']
+        relations_data = [
+            [
+                entities[rel['subject']]['head'].lower(),
+                rel['relation'].lower(),
+                entities[rel['object']]['head'].lower()
+            ]
+            for rel in graph['relations']
+        ]
+
+        return relations_data
 
 
 _tabulate_format = tabulate.TableFormat(
